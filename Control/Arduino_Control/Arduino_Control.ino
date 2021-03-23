c4d01b93f81c71868d4e97a08e17fa79d73c258d
Sajjad Al-Kazzaz <alkazzazsajjad@gmail.com>
2021-03-23 11:00:08 -0700

    Matlab PID function implementation and debugging

--- Control/Arduino_Control/Arduino_Control.ino
+++ Control/Arduino_Control/Arduino_Control.ino
@@ -6,13 +6,26 @@
 #define KP
 #define KD
 #define KI
+double gainA[3];
+double gainB[3];
+double gainC[3];
+double gainD[3];
+
 const double Tsample = 1/500;
 const int nhat = 12;
 const int P = 4/(Tsample*nhat);
-double errorSum = 0;
+
+double errorSumA = 0;
+double errorSumB = 0;
+double errorSumC = 0;
+double errorSumD = 0;
 
 // Pins:
-int Input = A1;        // Analog input pin for error
+int InputA = A1;        // Analog input pin for error
+int InputB = A1;
+int InputC = A1;
+int InputD = A1;
+
 int OutputA = A2;      // Analog output pin for the control signal
 int OutputB = A3; 
 int OutputC = A4; 
@@ -27,11 +40,15 @@ int M3[] = {};
 // Persistent:
 double PrevFilter[nhat] = {0};
 double PrevDerivative[nhat] = {0};
-double ept[nhat] = {0};
+double ept[nhat];
 double Temp = 0; 
 
 void setup() {
-  pinMode(Input, INPUT);
+  pinMode(InputA, INPUT);
+  pinMode(InputB, INPUT);
+  pinMode(InputC, INPUT);
+  pinMode(InputD, INPUT);
+  
   pinMode(OutputA, OUTPUT);
   pinMode(OutputB, OUTPUT);
   pinMode(OutputC, OUTPUT);
@@ -49,24 +66,31 @@ void setup() {
 }
 
 void loop() {
-  
+  // Call ISR
+  ISR(InputA, OutputA, gainA[], errorSumA);
 }
 
-int ISR() {
+bool ISR(in, out, gain, errorSum) {
   // Read position (from encoder) and calculate error
-  int PositionActual = analogRead(Input);
+  int PositionActual = analogRead(in);
   int error = PositionDesired - PositonActual;
 
   // Proportional
-  double ProportionalOutput = KP*error;
+  double ProportionalOutput = gain[0]*error;
 
   // Integral
   errorSum += error;
-  double IntegralOutput = KI*errorSum;
+  double IntegralOutput = gain[1]*errorSum;
 
   // Filtered derivative
   PrevDerivative[0] = error - Temp;
+  
   // Derivative output
+  double DerivativeOutput = 0;
+  for(n=0; n<nhat; n++) {
+    DerivativeOutput += PrevDerivative[n]*ept[n];
+  }
+  
   // Shift PrevDerivative:
   for(n=0; n<=nhat-1; n++){
     PrevDerivative(nhat + 1 - n) = PrevDerivative(nhat - n);
@@ -76,5 +100,9 @@ int ISR() {
   Temp = error;
 
   // Output
-  analogWrite(Output, ProportionalOutput + IntegralOutput + DerivativeOutput);
+  analogWrite(out, ProportionalOutput + IntegralOutput + DerivativeOutput*gain[2]);
+  if(error <= 0.1){
+    return true;
+  }
+  return false;
 }
